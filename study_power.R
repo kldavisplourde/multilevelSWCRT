0ffb6d03cdbec1d99b49db95ffe56af1bfb4c358
Kendra Davis-Plourde <kendra.davis727@gmail.com>
2021-07-21 10:59:07 -0400

    Update study_power.R
    alpha is already being used for ICCs

--- study_power.R
+++ study_power.R
@@ -4,11 +4,11 @@
 # INPUTS
 # delta: Intervention effect on link function scale
 # var.delta: Variance of intervention effect on link function scale
-# alpha: Type I error rate for t-test (default is 0.05)
+# typeI.error: Type I error rate for t-test (default is 0.05)
 # df: degrees of freedom for t-test
 
-study_power<-function(delta, var.delta, alpha=0.05, df=n-2){
-  tcrit<-qt(1-alpha/2, df=df)
+study_power<-function(delta, var.delta, typeI.error=0.05, df=n-2){
+  tcrit<-qt(1-typeI.error/2, df=df)
   power<-1-pt(tcrit, df, abs(delta)/sqrt(var.delta))
   return(power)
 }
