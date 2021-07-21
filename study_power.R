389a58cb57b1bdb44d295e3543945e2ce538bf6e
Kendra Davis-Plourde <kendra.davis727@gmail.com>
2021-07-21 10:52:57 -0400

    Noncentrality parameter added

--- study_power.R
+++ study_power.R
@@ -4,12 +4,11 @@
 # INPUTS
 # delta: Intervention effect on link function scale
 # var.delta: Variance of intervention effect on link function scale
-# typeI.error: Type I error rate for t-test (default is 0.05)
+# alpha: Type I error rate for t-test (default is 0.05)
 # df: degrees of freedom for t-test
 
-study_power<-function(delta,var.delta,typeI.error=0.05,df){
-  tE1<-qt(1-typeI.error/2, df=df)
-  tE2<-abs(delta)/sqrt(var.delta) - tE1
-  power<-pt(tE2, df=df)
+study_power<-function(delta, var.delta, alpha=0.05, df=n-2){
+  tcrit<-qt(1-alpha/2, df=df)
+  power<-1-pt(tcrit, df, abs(delta)/sqrt(var.delta))
   return(power)
 }
